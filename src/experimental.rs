597ba11b0ef2909cc0161809524a27ca717b976f
InnocentusLime <innocentuslime.help@gmail.com>
2025-11-16 20:27:20 +0200

    remove most experimental stuff

--- src/experimental.rs
+++ src/experimental.rs
@@ -2,9 +2,5 @@
 //! Some of them will eventually move to other modules,
 //! some will move into separate crates and some may just disappear.
 
-pub mod animation;
-pub mod camera;
-pub mod collections;
 pub mod coroutines;
 pub mod scene;
-pub mod state_machine;
